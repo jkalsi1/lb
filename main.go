a50bb573d7bb89ac7cf262e2e0694f8303b759b4
Jagan Kalsi <jkalsi@Jagans-MacBook-Air.local>
2026-03-11 18:19:10 -0700

    added a log to see whats going on

--- main.go
+++ main.go
@@ -128,6 +128,7 @@ func lb(w http.ResponseWriter, r *http.Request) {
 	peer := serverPool.GetNextPeer()
 	if peer != nil {
 		peer.ReverseProxy.ServeHTTP(w, r)
+		log.Printf("LB: Sucess! Request served by: %s", peer.URL)
 		return
 	}
 	http.Error(w, "Service not available", http.StatusServiceUnavailable)
